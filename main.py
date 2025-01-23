e0d3a8758b271a5753a42b66b81de1195c1c7bed
MarDoA <omarhabib30@gmail.com>
2025-01-23 08:28:04 +0200

    add movement and rotation

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    game_clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        player.update(dt)
+
         screen.fill((0,0,0))
+        player.draw(screen)
+        pygame.display.flip()
 
-
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
